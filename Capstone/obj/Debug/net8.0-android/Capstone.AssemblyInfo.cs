be2f80e4486fcebdf6a30f6b5d3c232df68fabc3
urmothermearey <rachel.pacot@gmail.com>
2024-03-28 14:43:20 +0800

    loginpage

--- Capstone/obj/Debug/net8.0-android/Capstone.AssemblyInfo.cs
+++ Capstone/obj/Debug/net8.0-android/Capstone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e4d3309dae3607e5335536d212aaa403b6783320")]
 [assembly: System.Reflection.AssemblyProductAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Capstone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
